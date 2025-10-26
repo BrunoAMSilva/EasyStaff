80b6b3203c2f6d9a55d787ae2f50ea165097bfe9
BrunoAMSilva <brunoamsilva@gmail.com>
2025-10-26 15:08:58 +0000

    fix: bass staff note positioning

--- src/utils/note-position.ts
+++ src/utils/note-position.ts
@@ -1,12 +1,19 @@
-export function getNotePosition(note: string): string {
+export function getNotePosition(note: string, clef: 'treble' | 'bass'): string {
   const notePositions: { [key: string]: string } = {
-    do: 'row-13',
-    re: 'row-12',
-    mi: 'row-11',
-    fa: 'row-10',
-    sol: 'row-9',
-    la: 'row-7',
-    si: 'row-5',
+    'treble-do': 'row-13',
+    'treble-re': 'row-12',
+    'treble-mi': 'row-11',
+    'treble-fa': 'row-10',
+    'treble-sol': 'row-9',
+    'treble-la': 'row-7',
+    'treble-si': 'row-5',
+    'bass-do': 'row-8',
+    'bass-re': 'row-7',
+    'bass-mi': 'row-6',
+    'bass-fa': 'row-5',
+    'bass-sol': 'row-4',
+    'bass-la': 'row-3',
+    'bass-si': 'row-2',
   };
-  return notePositions[note] || 'row-1'; // Default position if note not found
+  return notePositions[`${clef}-${note}`] || 'row-1'; // Default position if note not found
 }
