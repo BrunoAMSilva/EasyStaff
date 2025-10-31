4e7816f4f287d6d5c4e363bc9d437d43d87e8695
BrunoAMSilva <brunoamsilva@gmail.com>
2025-10-31 17:54:56 +0000

    fix: tweak colors

--- src/utils/note-colors.ts
+++ src/utils/note-colors.ts
@@ -13,12 +13,12 @@ export function getNoteColor(note: string): string {
 export function getNoteColorByStep(note: string): string {
   const noteColors: { [key: string]: string } = {
     "C": "bg-purple-700",
-    "D": "bg-orange-700",
-    "E": "bg-green-700",
-    "F": "bg-blue-700",
-    "G": "bg-yellow-700",
-    "A": "bg-red-700",
-    "B": "bg-pink-700",
+    "D": "bg-orange-600",
+    "E": "bg-lime-700",
+    "F": "bg-rose-800",
+    "G": "bg-indigo-800",
+    "A": "bg-pink-500",
+    "B": "bg-teal-700",
   };
   return noteColors[note] || "bg-gray-500"; // Default color if note not found
 }
